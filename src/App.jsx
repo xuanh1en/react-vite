f16372ccfb21e5488d5cbb669b0b4f106e9c4c2a
xuanh1en <nguyenxuanhien1506@gmail.com>
2024-08-03 17:07:54 +0700

    video 51

--- src/App.jsx
+++ src/App.jsx
@@ -10,46 +10,13 @@ import { Outlet } from 'react-router-dom';
 
 const App = () => {
 
-  const [todoList,setTodoList] = useState([])
 
-
-  const addNewTodo=(name)=>{
-    const newTodo={
-      id:randomIntFromInterval(1,100),
-      name:name
-    }
-    setTodoList([...todoList,newTodo])
-  }
-  const randomIntFromInterval=(min, max)=> { // min and max included  
-    return Math.floor(Math.random() * (max - min + 1) + min); 
-    }
-  
-    const deleteTodo=(id)=>{
-      const newTodo=todoList.filter(item=>item.id !==id)
-      setTodoList(newTodo)
-    }
   return (
     <>
       <Header/>
-      <div className="todo-container">
-        <div className="todo-title">Todo list</div>
-        <TodoNew
-          addNewTodo={addNewTodo}
-        />
-      {todoList.length >0 
-      ?<TodoData
-          todoList={todoList}
-          deleteTodo={deleteTodo}
-        />
-      :<div className='todo-image'>
-            <img src={reactLogo} className='logo' />
-          </div>
-      }
-      </div>
       <Outlet/>
       <Footer/>
     </>
-      
     )
       
 }
--- src/main.jsx
+++ src/main.jsx
@@ -11,12 +11,17 @@ import LoginPage from './pages/login.jsx';
 import RegisterPage from './pages/register.jsx';
 import UserPage from './pages/user.jsx';
 import ProductPage from './pages/product.jsx';
+import TodoApp from './components/todo/TodoApp.jsx';
 
   const router = createBrowserRouter([ 
     { 
     path: "/", 
     element:  <App />, 
     children:[
+          {
+            index: true,
+            element:<TodoApp/>
+          },
           {
             path:"/users",
             element: <UserPage />
